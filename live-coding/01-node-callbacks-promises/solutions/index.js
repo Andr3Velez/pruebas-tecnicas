56347af581fd14eae1af2d36c6bd0e5c9ff23e9f
Andre Velez <andrevelez151@hotmail.com>
2025-07-20 11:59:55 -0500

    todos los ejercicios solucionados

--- live-coding/01-node-callbacks-promises/solutions/index.js
+++ live-coding/01-node-callbacks-promises/solutions/index.js
@@ -1,8 +1,8 @@
-//1 - Arregla esta función para que el código posterior funcione como se espera:
-
 import net from "node:net";
-import fs from "fs:net";
-import fsp from "fs:net/promises";
+import fs from "node:fs";
+import fsp from "node:fs/promises";
+
+//1 - Arregla esta función para que el código posterior funcione como se espera:
 
 export const ping = (ip, callback) => {
   const startTime = process.hrtime();
@@ -42,3 +42,31 @@ export async function procesarArchivo() {
   const textoProcesado = contenido.toUpperCase();
   await fsp.writeFile("output.txt", textoProcesado);
 }
+
+// 4 - ¿Cómo mejorarías el siguiente código y por qué? Arregla los tests si es necesario:
+
+export async function leerArchivos() {
+  const [archivo1, archivo2, archivo3] = await Promise.allSettled([
+    fsp.readFile("../archivo1.txt", "utf8"),
+    fsp.readFile("../archivo2.txt", "utf8"),
+    fsp.readFile("../archivo3.txt", "utf8"),
+  ]);
+
+  return `${archivo1.value} ${archivo2.value} ${archivo3.value}`;
+}
+
+const sol = await leerArchivos();
+console.log(sol);
+
+// 5 - Escribe una funcion `delay` que retorne una promesa que se resuelva después de `n` milisegundos. Por ejemplo:
+
+export async function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
+delay(3000).then(() => console.log("Hola mundo"));
+// o..
+await delay(3000);
+console.log("Hola mundo");
